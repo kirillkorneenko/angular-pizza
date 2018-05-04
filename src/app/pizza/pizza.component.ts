d155bb8bd74f9c7d06c6cbdfcc20d556dec62cce
kirillkorneenko <kirill.korneenko@gmail.com>
2018-05-04 22:49:51 +0300

    adding an order

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PizzaService} from '../../services/pizza/pizza.service';
-import {SaucesService} from '../../services/sauce/sauces.service';
 import {SizepizzaService} from '../../services/sizepizza/sizepizza.service';
 import {AppComponent} from '../app.component';
+import {PastryService} from '../../services/pastry/pastry.service';
 
 @Component({
   selector: 'app-pizza',
@@ -13,13 +13,15 @@ export class PizzaComponent implements OnInit {
 
   data: Array<any>;
   sizes: Array<any>;
-  sizePizza = null;
+  pastryies: Array<any>;
 
-  constructor(private pizzaService: PizzaService, private sizeServise: SizepizzaService, private appComponent: AppComponent) { }
+
+  constructor(private pizzaService: PizzaService, private sizeServise: SizepizzaService, private appComponent: AppComponent, private pastryService: PastryService) { }
 
   ngOnInit() {
     this.getAll();
     this.getAllSize();
+    this.getAllPastry();
   }
 
   private getAll() {
@@ -34,23 +36,46 @@ export class PizzaComponent implements OnInit {
     this.sizeServise.getAllSize().subscribe(
       (res: any) => {
         this.sizes = res;
-        this.sizePizza = this.sizes[this.sizes.length - 1];
       }
     );
   }
 
-  public addToBasket(pizza, sizePizza) {
+  private getAllPastry() {
+    this.pastryService.getAllPastry().subscribe(
+      (res: any) => {
+        this.pastryies = res;
+      }
+    );
+  }
+
+  public addToBasket(pizza, sizePizza, pastry) {
     const pizzaOut = {
       uuid: Date.now(),
       id: '',
       name: '',
       size: '',
-      price: ''
+      pastry: '',
+      price: null
     };
+    const itemSize = this.sizes.find(i => i.size === sizePizza);
+    const itemPastry = this.pastryies.find(i => i.kind === pastry);
     pizzaOut.id = pizza.id;
     pizzaOut.name = pizza.name;
-    pizzaOut.size = sizePizza;
-    pizzaOut.price = pizza.price;
+    pizzaOut.size = itemSize;
+    pizzaOut.pastry = itemPastry;
+    pizzaOut.price = ((pizza.price * itemSize.coefficient)+ (itemPastry.price*itemSize.coefficient));
     this.appComponent.addPizza(pizzaOut);
+  }
+
+  public calcCall(pizz, size, pastry) {
+    const itemSize = this.sizes.find(i => i.size === size);
+    const itemPastry = this.pastryies.find(i => i.kind === pastry);
+    return ((pizz.caloricity * itemSize.coefficient)+(itemPastry.caloricity*itemSize.coefficient));
+  }
+
+  public calcPrice(pizz, size, pastry) {
+    const itemSize = this.sizes.find(i => i.size === size);
+    const itemPastry = this.pastryies.find(i => i.kind === pastry);
+    return ((pizz.price * itemSize.coefficient)+ (itemPastry.price*itemSize.coefficient));
   }
 }
